975dd8fd28e3c668f93223d3f9967cd1a0b65002
DucM1n <minercoc26@gmail.com>
2025-08-21 23:53:02 +0700

    Add readme.md and hello.py files

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+
+# This script prints "Hello, World!" to the console.
+# It is a simple demonstration of a Python script.
+# You can run this script by executing it in a terminal or command prompt.
